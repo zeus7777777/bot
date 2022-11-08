1c677b7adedd85efc57e5ff42c6a5225ab5a9d90
Zeus <zeus840703@gmail.com>
2022-11-08 20:39:57 +0800

    refine

--- a.py
+++ a.py
@@ -1,36 +1,53 @@
-target_hour = 19
-target_minute = 14
-end_hour = 19
-end_minute = 19
-
-import time
 import datetime
+import json
+import requests
+import time
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 
-def wait_until(hour, minute):
-    while True:
-        now = datetime.datetime.utcnow()
-        if now.hour == hour and now.minute == minute:
-            break
-        time.sleep(5)
+block_base = 3368160
+block_offset = 1440
 
-def break_cond():
-    now = datetime.datetime.utcnow()
-    return now.hour == end_hour and now.minute == end_minute
+offset = -2
+duration = 5
+
+def get_current_height():
+    try:
+        r = requests.get('https://nodes.wavesnodes.com/blocks/last')
+        current_height = int(json.loads(r.text)['height'])
+        return current_height
+    except:
+        return 0
+
+def wait_until(height):
+    print('waiting...')
+    last = 0
+    while True:
+        current = get_current_height()
+        if current != last:
+            print(current)
+            last = current
+        if  current >= height:
+            break
+        else:
+            time.sleep(15)
+
+current_height = get_current_height()
+target_height = block_base + block_offset * ((current_height - block_base) // block_offset + 1) + offset
+print('current_height', current_height)
+print('target_height', target_height)
 
 driver = webdriver.Chrome()
 driver.get('https://www.cmlab.csie.ntu.edu.tw/~zeus/src/')
 input('please login and click call contract, and then press any key to continue')
-print('waiting...')
 
 xpath = '//*[@id="root"]/div/div[3]/button[2]'
 driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
 
-wait_until(target_hour, target_minute)
+wait_until(target_height)
 
 while True:
     try:
@@ -39,8 +56,7 @@ while True:
         button = driver.find_element(By.XPATH, xpath)
         button.click()
         print('sent at', datetime.datetime.utcnow())
-        time.sleep(1)
     except:
         pass
-    if break_cond():
+    if get_current_height() >= target_height + duration:
         break
